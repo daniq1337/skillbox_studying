f3dd8fd0cee097840f8b0a6f51dbbcf4c41d7f53
Даниил <inenquser@gmail.com>
2024-02-27 17:04:54 +0300

    all tasks done

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -6,22 +6,29 @@ import simple_draw as sd
 sd.resolution = (1200, 600)
 
 # Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
-
+# point = sd.get_point(300, 300)
+# radius = 50
+# for _ in range(3):
+#     radius += 5
+#     sd.circle(center_position=point, radius=radius, width=2)
+# done!
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
 def bubble(point, step):
     radius = 50
     for _ in range(3):
         radius += step
-        sd.circle(center_position=point, radius=radius, width=2)
+        sd.circle(center_position=point, radius=radius, width=2, color=(sd.random_color()))
 # point = sd.get_point(300, 300)
 # bubble(point=point, step=10)
+# done!
 
 # Нарисовать 10 пузырьков в ряд
 
 # for x in range(100, 1100, 100):
 #     point = sd.get_point(x, 100)
 #     bubble(point=point, step=5)
+# done!
 
 # Нарисовать три ряда по 10 пузырьков
 
@@ -29,12 +36,15 @@ def bubble(point, step):
 #     for x in range(100, 1100, 100):
 #         point = sd.get_point(x, y)
 #         bubble(point=point, step=5)
+# done!
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
 for _ in range(100):
     point = sd.random_point()
     step = random.randint(2, 10)
     bubble(point=point, step=step)
+# done!
+# each task was successfully done. to run specific task, just uncomment it
 
 sd.pause()
 
