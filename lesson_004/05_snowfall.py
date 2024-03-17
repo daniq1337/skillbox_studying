1aa00c394ff86fa891537ed07f00e318daab94cf
Даниил <inenquser@gmail.com>
2024-03-17 17:41:49 +0300

    snowfall done

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -4,16 +4,9 @@ import simple_draw as sd
 
 
 # На основе кода из практической части реализовать снегопад:
-# - создать списки данных для отрисовки N снежинок
+# - создать списки данных для отрисовки N снежинок (x, y)
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
-
-# Постарался сделать более читаемым код.
-# Можно, пожалуйста, подробнее, что конкретно не читаемо было для Вас.
-# Как бы Вы это написали, а лучше на примере из 05_snowfall.py
-
-# Мое замечание касалось только перменной N. Сейчас код хорошо читается, у меня
-# замечания нет.
 
 # кол-во снежинок
 snowflakes_count = 20
@@ -38,8 +31,8 @@ for i in range(snowflakes_count):
     snowflakes[i] = {'length': sd.random_number(snowflake_size['min'], snowflake_size['max']),
                      'x': sd.random_number(0, sd.resolution[0]),
                      'y': y,
-                     'factor_a': sd.random_number(1, 10)/10,
-                     'factor_b': sd.random_number(1, 10)/10,
+                     'factor_a': sd.random_number(1, 10) / 10,
+                     'factor_b': sd.random_number(1, 10) / 10,
                      'factor_c': sd.random_number(1, 120)
                      }
 
@@ -75,7 +68,6 @@ while True:
 
     sd.finish_drawing()
     sd.sleep(0.1)
-
     if sd.user_want_exit():
         break
 
@@ -97,4 +89,3 @@ sd.pause()
 # Результат решения см https://youtu.be/XBx0JtxHiLg
 
 
-# Зачет!
