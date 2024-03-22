8c87a267a288205bff1be99d4c49ecf820956b68
Даниил <inenquser@gmail.com>
2024-03-22 15:58:28 +0300

    Стену можно делать в любом месте экрана, можно поменять её цвет и размеры

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -3,63 +3,68 @@
 # (цикл for)
 import simple_draw as sd
 
-# Нарисовать стену из кирпичей. Размер кирпича - 100х50
-# Использовать вложенные циклы for
-
 sd.resolution = (800, 800)
-sd.background_color = sd.COLOR_DARK_RED
+
+# размер стены
+wall_size_x = 500
+wall_size_y = 500
 
 # размер кирпича
-x = 100
-y = 50
+x = 50
+y = 25
 
+# полкирпича
 half_brick = x // 2
 
-brick_x_count = sd.resolution[0] // x
-brick_y_count = sd.resolution[1] // y
+# количество кирпичей в ряду
+brick_x_count = wall_size_x // x
 
-start_x = 0
-start_y = 0
-end_x = start_x + x
-end_y = start_y + y
+# количество рядов всего
+brick_y_count = wall_size_y // y
+
+# начальная точка по иксу
+x_for_each_row = 100
+
+start_x = x_for_each_row
+start_y = 100
 
 x_count = 0
 
+wall_left_bottom = sd.get_point(start_x, start_y)
+wall_right_top = sd.get_point(start_x + wall_size_x, start_y + wall_size_y )
+sd.rectangle(wall_left_bottom, wall_right_top, color=sd.COLOR_DARK_RED)
+
 for brick_y in range(brick_y_count):
 
-    # определяем кол-во кирпичей в ряду
-    if brick_y % 2 == 1:
-        x_count += 1
-    else:
+    # проверяем количество кирпичей в ряду
+    if brick_y % 2 == 0:
         x_count = brick_x_count
+    else:
+        x_count += 1
 
     for brick_x in range(x_count):
 
         if brick_y % 2 == 1:
             if brick_x == 0:
                 start_position = sd.get_point(start_x, start_y)
-                end_position = sd.get_point(end_x - half_brick, end_y)
-            elif brick_x == x_count-1:
+                end_position = sd.get_point(start_x + half_brick, start_y + y)
+            elif brick_x == x_count - 1:
                 start_position = sd.get_point(start_x - half_brick, start_y)
-                end_position = sd.get_point(end_x - x, end_y)
+                end_position = sd.get_point(start_x, start_y + y)
             else:
                 start_position = sd.get_point(start_x - half_brick, start_y)
-                end_position = sd.get_point(end_x-half_brick, end_y)
+                end_position = sd.get_point(start_x - half_brick + x, start_y + y)
         else:
             start_position = sd.get_point(start_x, start_y)
-            end_position = sd.get_point(end_x, end_y)
+            end_position = sd.get_point(start_x + x, start_y + y)
 
-        sd.rectangle(left_bottom=start_position, right_top=end_position, color=sd.COLOR_BLACK, width=1)
-
+        sd.rectangle(left_bottom=start_position, right_top=end_position, color=sd.COLOR_BLACK, width=2)
         start_x += x
-        end_x += x
 
-        sd.sleep(0.03)
+    # возвращаем кирпич в начало ряда
+    start_x = x_for_each_row
 
-    start_x = 0
-    end_x = x
-
-    start_y = end_y
-    end_y += y
+    # поднимаем ряд на один выше
+    start_y += y
 
 sd.pause()
