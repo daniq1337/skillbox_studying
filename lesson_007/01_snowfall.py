8dc0165a9710b1ebd37409707fa0111bfb57ba3c
Даниил <inenquser@gmail.com>
2024-04-05 16:24:16 +0300

    Падение одной снежинки через классы

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from random import randint
 
 import simple_draw as sd
 
@@ -7,11 +8,39 @@ import simple_draw as sd
 #  - отработку изменений координат
 #  - отрисовку
 
+sd.resolution = (1200, 800)
+
 
 class Snowflake:
-    pass
 
-    # TODO здесь ваш код
+    snowflake_size = {'min': 5, 'max': 20}
+
+    def __init__(self):
+        self.x = sd.random_number(0, sd.resolution[0])
+        self.y = sd.random_number(500, 700)
+        self.length = sd.random_number(Snowflake.snowflake_size['min'], Snowflake.snowflake_size['max'])
+        self.factor_a = sd.random_number(1, 10) / 10
+        self.factor_b = sd.random_number(1, 10) / 10
+        self.factor_c = sd.random_number(30, 60)
+
+    def draw(self, color=sd.COLOR_WHITE):
+        self.start_point = sd.get_point(x=self.x, y=self.y)
+        sd.snowflake(center=self.start_point,
+                     length=self.length,
+                     color=color,
+                     factor_a=self.factor_a,
+                     factor_b=self.factor_b,
+                     factor_c=self.factor_c)
+
+    def move(self):
+        self.x += sd.random_number(0, 2)
+        self.y -= Snowflake.snowflake_size['max'] + 1 - self.length
+
+    def clear_previous_picture(self, color=sd.background_color):
+        self.draw(color)
+
+    def can_fall(self):
+        return self.y > 0
 
 
 flake = Snowflake()
