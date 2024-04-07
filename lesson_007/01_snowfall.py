1c40b7668e4f4d01da8ee2d1207da847b6a75913
Даниил <inenquser@gmail.com>
2024-04-07 13:38:14 +0300

    финал снежинок тестовый

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
+from pprint import pprint
 
 import simple_draw as sd
-
 
 # Шаг 1: Реализовать падение снежинки через класс. Внести в методы:
 #  - создание снежинки с нужными параметрами
@@ -10,7 +10,6 @@ import simple_draw as sd
 
 sd.resolution = (1200, 800)
 snowflakes = {}
-tick = 0
 
 
 class Snowflake:
@@ -19,10 +18,10 @@ class Snowflake:
     def __init__(self):
         self.length = sd.random_number(Snowflake.size['min'], Snowflake.size['max'])
         self.x = sd.random_number(0, sd.resolution[0])
-        self.y = sd.randint(sd.resolution[1] - 100, sd.resolution[1] + 100)
+        self.y = sd.randint(sd.resolution[1] - 100, sd.resolution[1])
         self.factor_a = sd.random_number(1, 10) / 10
         self.factor_b = sd.random_number(1, 10) / 10
-        self.factor_c = sd.random_number(1, 120)
+        self.factor_c = sd.random_number(30, 70)
 
     def draw(self, color=sd.COLOR_WHITE):
         start_point = sd.get_point(x=self.x, y=self.y)
@@ -37,12 +36,12 @@ class Snowflake:
         self.draw(color)
 
     def move(self):
-        self.x += sd.random_number(0, 2)
+        self.x += sd.random_number(-2, 2)
         self.y -= Snowflake.size['max'] + 1 - self.length
 
 
 def run_snowfall(snowflakes_count=0):
-    """Запуск снегопада"""
+    """Запуск снегопада."""
     if len(snowflakes) != snowflakes_count:
         snowflakes[len(snowflakes)] = Snowflake()
 
@@ -55,20 +54,13 @@ def run_snowfall(snowflakes_count=0):
             snowflakes[num] = Snowflake()
 
 
-if __name__ == '__main__':
-    while True:
-        tick += 1
-        sd.start_drawing()
+while True:
+    sd.start_drawing()
 
-        if tick < 50:
-            run_snowfall(snowflakes_count=20)
-        elif tick > 50:
-            run_snowfall(snowflakes_count=50)
+    run_snowfall(snowflakes_count=20)
 
-        sd.sleep(0.05)
-        sd.finish_drawing()
+    sd.sleep(0.05)
+    sd.finish_drawing()
 
-        if sd.user_want_exit():
-            break
-
-# Зачет!
+    if sd.user_want_exit():
+        break
